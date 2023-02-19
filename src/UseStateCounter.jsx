a5c01ff2b7ac8a64d1637867e3f409f35a428925
Saliha <salihaoguz2011@gmail.com>
2023-02-19 19:36:07 +0000

    clear

--- src/UseStateCounter.jsx
+++ src/UseStateCounter.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 
 const UseStateCounter = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="container text-center mt-5">
       <h2> </h2>
-      <h1 className="display-4">COUNT:</h1>
-      <button className="btn btn-success me-4">INCREASE</button>
-      <button className="btn btn-warning me-4">CLEAR</button>
-      <button className=" btn btn-danger">DECREASE</button>
+      <h1 className="display-4">COUNT:{count}</h1>
+      <button onClick={increment} className="btn btn-success me-4">
+        INCREASE
+      </button>
+      <button onClick={() => setCount(0)} className="btn btn-warning me-4">
+        CLEAR
+      </button>
+      <button
+        onClick={() => count > 0 && setCount(count - 1)}
+        className=" btn btn-danger"
+      >
+        DECREASE
+      </button>
     </div>
   );
 };
