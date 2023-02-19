61d0e172c7a253d28d1ed075c0f494f76169f537
Saliha <salihaoguz2011@gmail.com>
2023-02-19 19:57:31 +0000

    personObject

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import UseStateCounter from "./UseStateCounter";
+import UseStateObject from "./UseStateObject";
 
 function App() {
   return (
     <div className="App">
       <UseStateCounter />
+      <UseStateObject />
     </div>
   );
 }
---
+++ src/UseStateObject.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: "Saliha",
+    surname: "Oguz",
+    salary: 6500,
+  });
+
+  const handleSalaryINc = () => {
+    setPerson({ ...person, salary: person.salary + 500 });
+  };
+  return (
+    <div className="container text-center mt-5">
+      <h2>
+        {person.name}
+        {person.surname}
+      </h2>
+      <h1 className="display-4">SALARY:${person.salary}</h1>
+      <button onClick={handleSalaryINc} className="btn btn-success me-4">
+        SALARY INCREASE
+      </button>
+      <button className="btn btn-warning me-4">CLEAR</button>
+      <button className=" btn btn-danger">SALARY DECREASE</button>
+    </div>
+  );
+};
+
+export default UseStateObject;
